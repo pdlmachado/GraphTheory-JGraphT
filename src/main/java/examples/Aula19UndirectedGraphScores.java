9629b9d2ad3d8a37606f2451639367a4102654d3
machado.patricia <patricia@computacao.ufcg.edu.br>
2019-11-06 16:52:21 -0300

    Aula 27

--- src/main/java/examples/Aula19UndirectedGraphScores.java
+++ src/main/java/examples/Aula19UndirectedGraphScores.java
@@ -10,6 +10,7 @@ import org.jgrapht.alg.scoring.ClosenessCentrality;
 import org.jgrapht.alg.scoring.ClusteringCoefficient;
 import org.jgrapht.alg.scoring.HarmonicCentrality;
 import org.jgrapht.alg.shortestpath.GraphMeasurer;
+import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.Multigraph;
 
 
@@ -17,31 +18,34 @@ public class Aula19UndirectedGraphScores {
 
 	public static void main(String[] args) {
 		
-		Graph<DefaultVertex, RelationshipEdge> ugraph = new Multigraph<>(RelationshipEdge.class);
-		MyJGraphTUtil.importGraphGML(ugraph, "./src/main/java/graphs/email-Eu-core.gml");
+		Graph<String, DefaultEdge> ugraph = new Multigraph<>(DefaultEdge.class);
+		MyJGraphTUtil.importDefaultGraphGML(ugraph, "./src/main/java/graphs/antcolony1000.gml");
+		
+		MyJGraphTUtil.printGraph(ugraph);
   	    
   	   	// Compute Metrics
   	   	System.out.println("-BETWEENESS CENTRALITY- ");
-  	   	BetweennessCentrality <DefaultVertex, RelationshipEdge> bc = 
+  	   	BetweennessCentrality <String, DefaultEdge> bc = 
   	   		new BetweennessCentrality <> (ugraph,true);
+  	   	System.out.println(bc.getScores());
   	   	MyJGraphTUtil.printOrderedVertexMeasures (bc.getScores(),0,true);
 
   	   	System.out.println("\n-ALPHA CENTRALITY- ");
-  	   	AlphaCentrality <DefaultVertex, RelationshipEdge> ac = 
+  	   	AlphaCentrality <String, DefaultEdge> ac = 
   	   		new AlphaCentrality <> (ugraph,0.001);
   	   	MyJGraphTUtil.printOrderedVertexMeasures (ac.getScores(),0,true);
 	    
   	   	System.out.println("\n-CLOSENESS CENTRALITY- ");
-  	   	ClosenessCentrality <DefaultVertex, RelationshipEdge> cc = 
+  	   	ClosenessCentrality <String, DefaultEdge> cc = 
 	   		new ClosenessCentrality <> (ugraph);
   	   	MyJGraphTUtil.printOrderedVertexMeasures (cc.getScores(),0,true);
 	    
   	   	System.out.println("\n-HARMONIC CENTRALITY- ");
-  	   	HarmonicCentrality <DefaultVertex, RelationshipEdge> hc = 
+  	   	HarmonicCentrality <String, DefaultEdge> hc = 
 	   		new HarmonicCentrality <> (ugraph);
   	   	MyJGraphTUtil.printOrderedVertexMeasures (hc.getScores(),0,true);
 	    
-  	   	ClusteringCoefficient <DefaultVertex,RelationshipEdge> cluster = 
+  	   	ClusteringCoefficient <String, DefaultEdge> cluster = 
 	    	new ClusteringCoefficient <> (ugraph);
   	   	System.out.println("\n\nCluster Coefficient: " + cluster.getGlobalClusteringCoefficient());
 	    
@@ -53,7 +57,7 @@ public class Aula19UndirectedGraphScores {
   	  	   	double density = (2*E) / (V*(V-1));
   	  	   	System.out.println("\nDENSITY: " + density);   		
   	   	}   
-  	   	GraphMeasurer <DefaultVertex, RelationshipEdge> g = new GraphMeasurer <> (ugraph);
+  	   	GraphMeasurer <String, DefaultEdge> g = new GraphMeasurer <> (ugraph);
 	   
   	   	System.out.println("\nDIAMETER: " + g.getDiameter());	
   	   	System.out.println("RADIUS: " + g.getRadius());

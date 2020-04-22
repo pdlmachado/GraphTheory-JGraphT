7672aa18e2c3bef4c93bfb5877c73bf5df56cce1
machado.patricia <patricia@computacao.ufcg.edu.br>
2020-04-22 10:07:12 -0300

    MeasureUtil Tests

--- src/test/java/util/MeasureUtilTest.java
+++ src/test/java/util/MeasureUtilTest.java
@@ -2,6 +2,9 @@ package util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -12,7 +15,6 @@ import org.jgrapht.Graph;
 import org.jgrapht.generate.NamedGraphGenerator;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
-import org.jgrapht.util.SupplierUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,18 +23,81 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class MeasureUtilTest {
 	
+	/////////////////////////
+	// Preparing Graph Data
+	
 	static Graph <String, DefaultEdge> nullgraph;
 	// Graphs with cycles
 	static Graph <Integer, DefaultEdge> bullgraph;
 	static Graph <Integer, DefaultEdge> petersengraph;
 	static Graph <Integer, DefaultEdge> butterflygraph;
-	static Graph<DefaultVertex, DefaultEdge> asso;
-	static Graph<DefaultVertex, DefaultEdge> disso;
+	static Graph<String, DefaultEdge> asso;
+	static String assof = 	"a,b\n" + // "./src/main/java/graphs/assortativity.gml"
+							"b,c\n" +
+							"c,f\n" +
+							"f,d\n" +
+							"f,e\n" +
+							"f,g\n" +
+							"d,e\n" +
+							"d,g\n" +
+							"e,g\n" +
+							"g,i\n" +
+							"i,h\n";
+	static Graph<String, DefaultEdge> disso;
+	static String dissof = 	"a,d\n" + // "./src/main/java/graphs/dissortativity.gml"
+							"b,d\n" +
+							"c,d\n" +
+							"e,d\n" +
+							"f,d\n" +
+							"d,g\n" +
+							"g,h\n" +
+							"g,l\n" +
+							"l,m\n" +
+							"l,n\n" +
+							"l,j\n" +
+							"l,i\n" +
+							"l,h\n";
 	// Trees
 	static Graph <Integer, DefaultEdge> clawgraph; 
-    static Graph <DefaultVertex, DefaultEdge> tree;
-    static Graph <DefaultVertex, DefaultEdge> cent;
+    static Graph <String, DefaultEdge> tree;
+    static String treef = 	"a,b\n" + // "./src/main/java/graphs/treec.gml"
+    						"b,c\n" +
+    						"c,d\n" +
+    						"d,e\n" +
+    						"e,f\n" +
+    						"e,g\n" +
+    						"e,h\n" +
+    						"h,i\n" +
+    						"i,j\n";
+    static Graph <String, DefaultEdge> cent;
+    static String centf = 	"1,2\n" +	//"./src/main/java/graphs/centroids.gml"
+    						"1,5\n" +
+    						"1,6\n" +
+    						"2,3\n" +
+    						"2,4\n" +
+    						"6,7\n" +
+    						"6,8\n" +
+    						"8,9\n" +
+    						"8,10\n";
+    
+    static Graph<String,DefaultEdge> readGraph 
+    	(Graph <String, DefaultEdge> g, String input) {
+    	StringReader r = new StringReader(input);
+    	try (BufferedReader br = new BufferedReader(r)) {
+    		String line = "";
 
+    		while ((line = br.readLine()) != null) {
+    			String [] attributes = line.split(",");
+    			g.addVertex(attributes[0]);
+    			g.addVertex(attributes[1]);
+    			g.addEdge(attributes[0], attributes[1]);
+    		}
+    	} catch (IOException e) {
+			// TODO Auto-generated catch block
+    		System.out.println("não criei");
+		}
+    	return g;
+    }
 	
 	@BeforeAll
 	static void beforeAll () {
@@ -41,20 +106,19 @@ class MeasureUtilTest {
 		petersengraph = NamedGraphGenerator.petersenGraph();
 		clawgraph = NamedGraphGenerator.clawGraph(); 
 		butterflygraph = NamedGraphGenerator.butterflyGraph(); 
-		asso = new SimpleGraph <> (VertexEdgeUtil.createDefaultVertexSupplier(), 
-	            SupplierUtil.createDefaultEdgeSupplier(), false);
-		ImportUtil.importDefaultGraphGML(asso, "./src/main/java/graphs/assortativity.gml");
-		disso = new SimpleGraph <> (VertexEdgeUtil.createDefaultVertexSupplier(), 
-	            SupplierUtil.createDefaultEdgeSupplier(), false);
-		ImportUtil.importDefaultGraphGML(disso, "./src/main/java/graphs/dissortativity.gml");
-		tree = new SimpleGraph <> (VertexEdgeUtil.createDefaultVertexSupplier(), 
-	            SupplierUtil.createDefaultEdgeSupplier(), false);
-		ImportUtil.importDefaultGraphGML(tree, "./src/main/java/graphs/treec.gml");
-		cent = new SimpleGraph <> (VertexEdgeUtil.createDefaultVertexSupplier(), 
-	            SupplierUtil.createDefaultEdgeSupplier(), false);
-		ImportUtil.importDefaultGraphGML(cent, "./src/main/java/graphs/centroids.gml");
+		asso = new SimpleGraph <> (DefaultEdge.class);
+		asso = readGraph(asso, assof);
+		disso = new SimpleGraph <> (DefaultEdge.class);
+		disso = readGraph(disso, dissof);
+		tree = new SimpleGraph <> (DefaultEdge.class);
+		tree = readGraph(tree,treef);		
+		cent = new SimpleGraph <> (DefaultEdge.class);
+		cent = readGraph(cent, centf);
 	}
 
+	/////////////////////////////
+	// Test Cases
+	
 	// assortativityCoefficient
 	static Stream <Arguments> assortativityCoefficientProvider () {
 		return Stream.of(
@@ -85,11 +149,11 @@ class MeasureUtilTest {
 	static Stream <Arguments> getTreeCentroidPointsProvider () {
 		Set <Integer> vclaw = new HashSet<Integer> ();
 		vclaw.add(new Integer(0));
-		Set <DefaultVertex> vtree = new HashSet<DefaultVertex> ();
-		vtree.add(VertexEdgeUtil.getVertexfromLabel(tree.vertexSet(), "e"));
-		Set <DefaultVertex> vcent = new HashSet<DefaultVertex> ();
-		vcent.add(VertexEdgeUtil.getVertexfromLabel(cent.vertexSet(), "1"));
-		vcent.add(VertexEdgeUtil.getVertexfromLabel(cent.vertexSet(), "6"));
+		Set <String> vtree = new HashSet<String> ();
+		vtree.add("e");
+		Set <String> vcent = new HashSet<String> ();
+		vcent.add("1");
+		vcent.add("6");
 		return Stream.of(
 				Arguments.of(nullgraph, new HashSet<Object>()),
 				Arguments.of(petersengraph, new HashSet <Integer>()),
@@ -113,30 +177,18 @@ class MeasureUtilTest {
 	
 	//getCutPointWeights
 	static Stream <Arguments> getCutPointWeightsProvider () {
-		Set<DefaultVertex> Vtree = tree.vertexSet();
-		DefaultVertex b = VertexEdgeUtil.getVertexfromLabel(Vtree, "b");
-		DefaultVertex d = VertexEdgeUtil.getVertexfromLabel(Vtree, "d");
-		DefaultVertex e = VertexEdgeUtil.getVertexfromLabel(Vtree, "e");
-		DefaultVertex h = VertexEdgeUtil.getVertexfromLabel(Vtree, "h");
-		DefaultVertex i = VertexEdgeUtil.getVertexfromLabel(Vtree, "i");
-		DefaultVertex c = VertexEdgeUtil.getVertexfromLabel(Vtree, "c");
-		Map <DefaultVertex, Double> mtree = new HashMap <> ();
-		mtree.put(b,8.0);
-		mtree.put(d,6.0);
-		mtree.put(e,4.0);
-		mtree.put(h,7.0);
-		mtree.put(i,8.0);
-		mtree.put(c,7.0);
-		Set<DefaultVertex> Vcent = cent.vertexSet();
-		DefaultVertex v2 = VertexEdgeUtil.getVertexfromLabel(Vcent, "2");
-		DefaultVertex v6 = VertexEdgeUtil.getVertexfromLabel(Vcent, "6");
-		DefaultVertex v8 = VertexEdgeUtil.getVertexfromLabel(Vcent, "8");
-		DefaultVertex v1 = VertexEdgeUtil.getVertexfromLabel(Vcent, "1");
-		Map <DefaultVertex, Double> mcent = new HashMap <> ();
-		mcent.put(v2,7.0);
-		mcent.put(v6,5.0);
-		mcent.put(v8,7.0);
-		mcent.put(v1,5.0);
+		Map <String, Double> mtree = new HashMap <> ();
+		mtree.put("b",8.0);
+		mtree.put("d",6.0);
+		mtree.put("e",4.0);
+		mtree.put("h",7.0);
+		mtree.put("i",8.0);
+		mtree.put("c",7.0);
+		Map <String, Double> mcent = new HashMap <> ();
+		mcent.put("2",7.0);
+		mcent.put("6",5.0);
+		mcent.put("8",7.0);
+		mcent.put("1",5.0);
 		return Stream.of(
 				Arguments.of(nullgraph,new HashMap <> ()),
 				Arguments.of(tree, mtree),

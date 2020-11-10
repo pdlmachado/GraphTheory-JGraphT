ea679ef59e50d7bcc132f838418b5f3a47053ba3
machadopatricia <patricia@dsc.ufcg.edu.br>
2020-11-10 15:22:48 -0300

    Aula 21

--- src/main/java/classexamples/Aula21SpanningTree.java
+++ src/main/java/classexamples/Aula21SpanningTree.java
@@ -9,13 +9,18 @@ import org.jgrapht.generate.GnmRandomGraphGenerator;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 import org.jgrapht.util.SupplierUtil;
-import java.lang.Math;
 
+import util.PrintUtil;
+
+import java.lang.Math;
+import java.util.Random;
 
 public class Aula21SpanningTree {
+	
+	private static final String NL = System.getProperty("line.separator");
+	
 	public static void main(String[] args) {
 		
-
 		int e = 8; //Number of Edged
 		int v = 6; //Number of Vertexes
 
@@ -25,20 +30,27 @@ public class Aula21SpanningTree {
 				new SimpleWeightedGraph <> 
 		            (SupplierUtil.createStringSupplier(),SupplierUtil.createDefaultWeightedEdgeSupplier());
 		genGraph.generateGraph(graph);
-				
+		Random rd = new Random();
+		graph.edgeSet().stream().forEach(i -> graph.setEdgeWeight(i,rd.nextInt(20)*1.0));
+		
+		PrintUtil.printWeightedGraph(graph, "Graph: ");
+		
 		BoruvkaMinimumSpanningTree <String,DefaultWeightedEdge> sp1 = 
   	    		new BoruvkaMinimumSpanningTree <> (graph);
-        System.out.println("\nBoruvkaMinimumSpanningTree (O((E+V)logV)): \n" + sp1.getSpanningTree().getWeight());
-        System.out.println("Computational Complexity: " + (e+v)*Math.log(v));
+        System.out.println(NL + "BoruvkaMinimumSpanningTree (O((E+V)logV)): " + NL + 
+        					sp1.getSpanningTree() + NL +
+        					"Computational Complexity Factor: " + (e+v)*Math.log(v));
         
   	    KruskalMinimumSpanningTree <String,DefaultWeightedEdge> sp2 = 
   	    		new KruskalMinimumSpanningTree <> (graph);
-        System.out.println("\nKruskalMinimumSpanningTree (O(ElogE)): \n" + sp2.getSpanningTree().getWeight());
-        System.out.println("Computational Complexity: " + e*Math.log(e));
+        System.out.println(NL + "KruskalMinimumSpanningTree (O(ElogE)): " + NL + 
+		           			sp2.getSpanningTree() + NL +
+		           			"Computational Complexity Factor: " + e*Math.log(e)); 
   	 
   	    PrimMinimumSpanningTree <String,DefaultWeightedEdge> sp3 = 
   	    		new PrimMinimumSpanningTree <> (graph);
-        System.out.println("\nPrimMinimumSpanningTree (O(|E|+|V|log(|V|))): \n" + sp3.getSpanningTree().getWeight());
-        System.out.println("Computational Complexity: " + (e+(v*Math.log(v))));
+        System.out.println(NL + "PrimMinimumSpanningTree (O(|E|+|V|log(|V|))):" +  NL +
+        					sp3.getSpanningTree() + NL +
+        					"Computational Complexity Factor: " + (e+(v*Math.log(v))) + NL);
    	}
 }
--- src/main/java/classexamples/Aula21TwoApproxTSP.java
+++ src/main/java/classexamples/Aula21TwoApproxTSP.java
@@ -6,6 +6,8 @@ import org.jgrapht.alg.tour.TwoApproxMetricTSP;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
 
+import util.PrintUtil;
+
 public class Aula21TwoApproxTSP {
     // 2-algoritmo de aproximação para problema TSP
 	
@@ -22,8 +24,10 @@ public class Aula21TwoApproxTSP {
 	    g.setEdgeWeight(g.addEdge("b","c"),3);
 	    g.setEdgeWeight(g.addEdge("b","d"),5);
 	    g.setEdgeWeight(g.addEdge("d","c"),6);
+	    PrintUtil.printWeightedGraph(g, "Graph: ");
         TwoApproxMetricTSP <String,DefaultWeightedEdge> t = new TwoApproxMetricTSP <> ();
-        System.out.println(t.getTour(g));
-        System.out.println((t.getTour(g)).getWeight());	    
+	    System.out.println("Minimum weight Hamiltonian Cycle:");
+	      System.out.println("C = " + t.getTour(g));
+	      System.out.println("w(C) =  " + (t.getTour(g)).getWeight());	    
 	}   
 }

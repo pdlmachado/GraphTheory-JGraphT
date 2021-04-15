c8d70dce432f75c1ddaf89f7637908c30cac2994
machado.patricia <patricia@computacao.ufcg.edu.br>
2021-04-15 10:33:08 -0300

    py

--- src/main/java/python/treeutil.py
+++ src/main/java/python/treeutil.py
@@ -1,9 +1,56 @@
-import jgrapht
-from random import randint
+"""# Funções"""
+
+from jgrapht.algorithms.shortestpaths import yen_k_loopless
+############################
+### Funções auxiliares para árvores enraizadas
+def parent (v,rtree):
+  inedges = list(rtree.inedges_of(v))
+  if len(inedges) == 1:
+    return rtree.edge_source(inedges[0])
+  else: # todo vértice tem apenas um predecessor em uma árvore enraizada
+    return None
+
+def children (v,tree):
+  oute = tree.outedges_of(v)
+  return [tree.edge_target(e) for e in oute]
+
+def is_root(v,tree):
+  return list(tree.inedges_of(v))==[]
+
+def is_leaf(v,tree):
+  return list(tree.outedges_of(v))==[]
+  
+def level(tree,root,v):
+  return len(next(yen_k_loopless(tree,root,v,1)).edges)
+
+def dist(g,v1,v2):
+  return len(next(yen_k_loopless(g,v1,v2,1)).edges)
+
+def get_rootedTree(tree,root):
+  if not is_tree(tree):
+    return None
+  # cria instância do grafo orientado com opção dag=True
+  rtree = jgrapht.create_graph(dag=True,weighted=False)
+  # adiciona vértices no grafo
+  rtree.add_vertices_from(tree.vertices)
+  # adiciona arcos no grafo a partir das arestas de tree
+  # considerando como source o terminal com menor nível (distância de root)
+  for e in tree.edges:
+    s = tree.edge_source(e)
+    t = tree.edge_target(e)
+    if level(tree,root,s) < level(tree,root,t):
+      rtree.add_edge(s,t)
+    else:
+      rtree.add_edge(t,s)
+  return rtree
+
 # Constroi uma árvore de busca em largura
 # Algoritmo BFS visto nas notas de aula, retornando árvore ao invés da função predecessor
 # Retorna uma árvore orientada, a função nível e a função tempo
+from random import randint
 def bfs (g,root):
+  if not root in g.vertices:
+    return None,None,None
   i = 0
   Q = [] # Lista que representa a fila Q
   i = i+1
@@ -32,11 +79,14 @@ def bfs (g,root):
     else:
       Q.pop(0) # remove x da cabeça da fila Q
   return tree,l,t
-  
+
 # Constroi uma árvore de busca em profundidade
 # Algoritmo DFS apresentado nas notas de aula
 # Retorna uma árvore DFS orientada e as funções tempo
+from random import randint
 def dfs (g,root):
+  if not root in g.vertices:
+    return None,None,None
   f = {} # função tempo de inclusão na árvore
   t = {} # função tempo de saída da pilha
   i = 0
@@ -63,44 +113,4 @@ def dfs (g,root):
       t[x] = i
       S.pop(0) #remove x do topo
   return tree,f,t
-
-############################
-### Funções auxiliares para árvores enraizadas
-def parent (v,rtree):
-  inedges = list(rtree.inedges_of(v))
-  if len(inedges) == 1:
-    return rtree.edge_source(inedges[0])
-  else: # todo vértice tem apenas um predecessor em uma árvore enraizada
-    return None
-
-def children (v,tree):
-  oute = tree.outedges_of(v)
-  return [tree.edge_target(e) for e in oute]
-
-def is_root(v,tree):
-  return list(tree.inedges_of(v))==[]
-
-def is_leaf(v,tree):
-  return list(tree.outedges_of(v))==[]
-  
-def level(tree,root,v):
-  return len(next(yen_k_loopless(tree,root,v,1)).edges)
-
-def get_rootedTree(tree,root):
-  if not is_tree(tree):
-    return None
-  # cria instância do grafo orientado com opção dag=True
-  rtree = jgrapht.create_graph(dag=True,weighted=False)
-  # adiciona vértices no grafo
-  rtree.add_vertices_from(tree.vertices)
-  # adiciona arcos no grafo a partir das arestas de tree
-  # considerando como source o terminal com menor nível (distância de root)
-  for e in tree.edges:
-    s = tree.edge_source(e)
-    t = tree.edge_target(e)
-    if level(tree,root,s) < level(tree,root,t):
-      rtree.add_edge(s,t)
-    else:
-      rtree.add_edge(t,s)
-  return rtree
   

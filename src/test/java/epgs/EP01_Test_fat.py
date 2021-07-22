9eed764cf2edffed9a740d78ea418c740761d9f3
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2021-07-22 14:58:14 -0300

    Update EP01_Test_fat.py

--- src/test/java/epgs/EP01_Test_fat.py
+++ src/test/java/epgs/EP01_Test_fat.py
@@ -1,3 +1,24 @@
+# From: https://eli.thegreenplace.net/2011/08/02/python-unit-testing-parametrized-test-cases
+class ParametrizedTestCase(unittest.TestCase):
+    """ TestCase classes that want to be parametrized should
+        inherit from this class.
+    """
+    def __init__(self, methodName='runTest', param=None):
+        super(ParametrizedTestCase, self).__init__(methodName)
+        self.param = param
+
+    @staticmethod
+    def parametrize(testcase_klass, param=None):
+        """ Create a suite containing all tests taken from the given
+            subclass, passing them the parameter 'param'.
+        """
+        testloader = unittest.TestLoader()
+        testnames = testloader.getTestCaseNames(testcase_klass)
+        suite = unittest.TestSuite()
+        for name in testnames:
+            suite.addTest(testcase_klass(name, param=param))
+        return suite
+    
 # Test Data
 toy1 = """digraph "toy1.jar" {
     // Path: toy1.jar

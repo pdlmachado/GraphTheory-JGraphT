2e44f1efd7c6c08111b8b97db2aab13f91d0784e
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2021-07-27 14:30:28 -0300

    Update Test_quarteiroes.py

--- src/test/java/epgs/Test_quarteiroes.py
+++ src/test/java/epgs/Test_quarteiroes.py
@@ -21,11 +21,51 @@ class ParametrizedTestCase(unittest.TestCase):
         return suite
     
 # Test Data
-
 import jgrapht
+
+g1 = jgrapht.create_graph(directed=False, weighted=True)
+g1.add_vertices_from([x for x in range(0,4)])
+g1.add_edge(0,1,edge=0)
+g1.add_edge(0,2,edge=1)
+g1.add_edge(0,3,edge=2)
+g1.add_edge(1,3,edge=3)
+g1.add_edge(2,3,edge=4)
+g1.set_edge_weight(0,10)
+
+# Grafo não direcionado conectado
+g2 = jgrapht.create_graph(directed=False, weighted=True)
+g2.add_vertices_from([x for x in range(9)])
+g2.add_edge(0,1,edge=0)
+g2.add_edge(0,2,edge=1)
+g2.add_edge(1,3,edge=2)
+g2.add_edge(1,8,edge=3)
+g2.add_edge(2,7,edge=4)
+g2.add_edge(2,6,edge=5)
+g2.add_edge(2,4,edge=6)
+g2.add_edge(3,5,edge=7)
+g2.add_edge(6,7,edge=8)
+g2.add_edge(7,8,edge=9)
+g2.add_edge(5,1,edge=10)
+g2.set_edge_weight(10,10)
+
+g3 = jgrapht.create_graph(directed=False, weighted=True)
+
 class Test_quarteiroes(ParametrizedTestCase):
 
   def test_valid01 (self):
-    pass
-    
-params = []
+    f,g,l,expected = self.param
+    try:
+      sresult = map(lambda x : sorted(x),f(g,l))
+      self.assertCountEqual(sresult,expected)
+    except:
+      self.assertTrue(f(g,l) is None and (g is None or l < 0))
+
+params = [[g1,11,[[0,1,3],[0,2,3]]],
+          [g1,1,[[0,2,3]]],
+          [g1,0,[]],
+          [g2,1,[[2,6,7]]],
+          [g2,10,[[1,3,5],[2,6,7]]],
+          [g3,20,[]],
+          [None,9,None],
+          [g1,-5,None],
+          ]

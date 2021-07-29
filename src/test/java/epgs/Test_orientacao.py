d1826501b8995a2c2d2c1e48501842fcb7479591
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2021-07-28 22:02:52 -0300

    Update Test_orientacao.py

--- src/test/java/epgs/Test_orientacao.py
+++ src/test/java/epgs/Test_orientacao.py
@@ -21,11 +21,71 @@ class ParametrizedTestCase(unittest.TestCase):
         return suite
     
 # Test Data
-
 import jgrapht
-class Test_orientacao(ParametrizedTestCase):
 
+g1 = jgrapht.create_graph(directed=False, weighted=False)
+g1.add_vertices_from([x for x in range(0,4)])
+g1.add_edge(0,1,edge=0)
+g1.add_edge(0,2,edge=1)
+g1.add_edge(0,3,edge=2)
+g1.add_edge(1,3,edge=3)
+g1.add_edge(2,3,edge=4)
+
+g2 = jgrapht.create_graph(directed=False, weighted=False)
+g2.add_vertices_from([x for x in range(9)])
+g2.add_edge(0,1,edge=0)
+g2.add_edge(0,2,edge=1)
+g2.add_edge(1,3,edge=2)
+g2.add_edge(1,8,edge=3)
+g2.add_edge(2,7,edge=4)
+g2.add_edge(2,6,edge=5)
+g2.add_edge(2,4,edge=6)
+g2.add_edge(3,5,edge=7)
+g2.add_edge(6,7,edge=8)
+g2.add_edge(7,8,edge=9)
+
+g3 = jgrapht.create_graph(directed=False, weighted=False)
+
+g4 = jgrapht.create_graph(directed=False, weighted=False)
+g4.add_vertices_from([x for x in range(9)])
+g4.add_edge(0,1,edge=0)
+g4.add_edge(0,2,edge=1)
+g4.add_edge(1,3,edge=2)
+g4.add_edge(1,8,edge=3)
+g4.add_edge(2,7,edge=4)
+g4.add_edge(2,6,edge=5)
+g4.add_edge(2,4,edge=6)
+g4.add_edge(3,5,edge=7)
+g4.add_edge(6,7,edge=8)
+g4.add_edge(7,8,edge=9)
+g4.add_edge(4,5,edge=10)
+
+g5 = jgrapht.create_graph(directed=False,weighted=False) 
+jgrapht.generators.complete_graph(g5,9)
+
+g6 = jgrapht.create_graph(directed=False,weighted=False) 
+jgrapht.generators.hypercube(g,8)
+
+g7 = jgrapht.create_graph(directed=False,weighted=False) 
+jgrapht.generators.wheel(g7,8)
+
+from jgrapht.algorithms.connectivity import is_strongly_connected_kosaraju
+class Test_orientacao(ParametrizedTestCase):
   def test_valid01 (self):
-    pass
+    f,g=self.param
+    d=f(g)
+    try:
+      cond,componentes = is_strongly_connected_kosaraju(d)
+      self.assertTrue(cond)
+    except:
+      self.assertTrue(d is None)
     
-params = []
+params = [[g1],
+          [g2],
+          [g3],
+          [g4],
+          [g5],
+          [g6],
+          [g7],
+          [None]
+          ]

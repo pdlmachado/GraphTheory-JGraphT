3fd38a8d3eb5223f549d18ea4d24f836a2d8a1cb
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2022-03-02 17:31:13 -0300

    Create Test_branches.py

---
+++ src/test/java/epgs/Test_branches.py
@@ -0,0 +1,43 @@
+import unittest
+# From: https://eli.thegreenplace.net/2011/08/02/python-unit-testing-parametrized-test-cases
+class ParametrizedTestCase(unittest.TestCase):
+    """ TestCase classes that want to be parametrized should
+        inherit from this class.
+    """
+    def __init__(self, methodName='runTest', param=None):
+        super(ParametrizedTestCase, self).__init__(methodName)
+        self.param = param
+
+    @staticmethod
+    def parametrize(testcase_klass, param=None):
+        """ Create a suite containing all tests taken from the given
+            subclass, passing them the parameter 'param'.
+        """
+        testloader = unittest.TestLoader()
+        testnames = testloader.getTestCaseNames(testcase_klass)
+        suite = unittest.TestSuite()
+        for name in testnames:
+            suite.addTest(testcase_klass(name, param=param))
+        return suite
+    
+# Test Data
+import jgrapht
+
+t1 = jgrapht.create_graph(directed=False, weighted=False)
+t1.add_vertices_from([x for x in range(0,6)])
+t1.add_edge(0,1,edge=0)
+t1.add_edge(0,2,edge=1)
+t1.add_edge(0,3,edge=2)
+t1.add_edge(1,4,edge=3)
+t1.add_edge(4,5,edge=4)
+
+t3 = jgrapht.create_graph(directed=False, weighted=False)
+
+class Test_branches1(ParametrizedTestCase):
+  def test_valid (self):
+    f,t,r,b = self.param
+    result = f(t,r)
+    print(result)
+    self.assertEqual(result,b)
+
+params = [[t1,0,[[0, 1, 4, 5], [0, 2], [0, 3]]] ]

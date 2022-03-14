9c34e4eea3350245bb8cf494307245ffb8793135
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2022-03-14 11:43:33 -0300

    Update Test_linkusage.py

--- src/test/java/epgs/Test_linkusage.py
+++ src/test/java/epgs/Test_linkusage.py
@@ -34,7 +34,7 @@ g1.set_edge_weight(0,4)
 g1.set_edge_weight(1,6)
 g1.set_edge_weight(2,1)
 g1.set_edge_weight(3,9)
-g1.set_edge_weight(4,7)
+g1.set_edge_weight(4,8)
 
 # Grafo não direcionado conectado
 g2 = jgrapht.create_graph(directed=False, weighted=True)

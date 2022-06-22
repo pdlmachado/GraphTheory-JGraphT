9d3bcd07a6da9ea2e454a896ac3a72e5c8f40aad
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2022-06-22 11:07:31 -0300

    Update Test_existem_caminhos_disjuntos.py

--- src/test/java/epgs/Test_existem_caminhos_disjuntos.py
+++ src/test/java/epgs/Test_existem_caminhos_disjuntos.py
@@ -23,16 +23,18 @@ class ParametrizedTestCase(unittest.TestCase):
 # Test Data
 import jgrapht
 
-d1 = jgrapht.create_graph(directed=True, weighted=False, dag=True)
+d0 = jgrapht.create_graph(directed=True, weighted=False)
+d0.add_vertex(0)
+
+d1 = jgrapht.create_graph(directed=True, weighted=False)
 d1.add_vertices_from([x for x in range(0,6)])
 d1.add_edge(0,1,edge=0)
 d1.add_edge(0,2,edge=1)
 d1.add_edge(3,0,edge=2)
-d1.add_edge(1,4,edge=3)
+d1.add_edge(1,3,edge=3)
 d1.add_edge(4,5,edge=4)
 
-# Árvore enraizada (dag)
-d2 = jgrapht.create_graph(directed=True, weighted=False, dag=True)
+d2 = jgrapht.create_graph(directed=True, weighted=False)
 d2.add_vertices_from([x for x in range(8)])
 d2.add_edge(0,1,edge=0)
 d2.add_edge(0,2,edge=1)
@@ -42,22 +44,24 @@ d2.add_edge(2,4,edge=4)
 d2.add_edge(5,3,edge=5)
 d2.add_edge(7,6,edge=6)
 d2.add_edge(7,4,edge=7)
+d2.add_edge(4,3,edge=8)
 
-# Árvore comum
-d3 = jgrapht.create_graph(directed=True, weighted=False, dag=True)
-d3.add_vertices_from([x for x in range(9)])
+d3 = jgrapht.create_graph(directed=True, weighted=False)
+d3.add_vertices_from([x for x in range(8)])
 d3.add_edge(0,1,edge=0)
 d3.add_edge(0,2,edge=1)
-d3.add_edge(1,3,edge=2)
+d3.add_edge(3,1,edge=2)
 d3.add_edge(2,7,edge=3)
-d3.add_edge(4,2,edge=4)
-d3.add_edge(3,5,edge=5)
+d3.add_edge(2,4,edge=4)
+d3.add_edge(5,3,edge=5)
 d3.add_edge(7,6,edge=6)
-d3.add_edge(7,8,edge=7)
-
-d4 = jgrapht.create_graph(directed=True, weighted=False, dag=True)
-d4.add_vertex(20)
-
+d3.add_edge(2,5,edge=7)
+d3.add_edge(7,4,edge=8)
+d3.add_edge(4,3,edge=9)
+d3.add_edge(5,4,edge=10)
+d3.add_edge(7,3,edge=11)
+d3.add_edge(4,0,edge=13)
+d3.add_edge(6,5,edge=14)
 
 class Test_existem_caminhos_disjuntos(ParametrizedTestCase):
   def test_base (self):
@@ -66,5 +70,13 @@ class Test_existem_caminhos_disjuntos(ParametrizedTestCase):
     self.assertEqual(result,er)
     self.assertCountEqual(caminhos,ec)
 
-params = [[d1,0,1,1,[]]
+params = [[d0,0,0,0,[]],
+          [d1,0,3,1,[]],
+          [d1,0,4,0,[]],
+          [d2,0,3,1,[]],
+          [d2,2,3,1,[]],
+          [d2,2,4,2,[[2, 4], [2, 7, 4]]],
+          [d3,2,4,2,[[2, 4], [2, 7, 4], [2, 5, 4]]],
+          [d3,7,0,1,[]],
+          [d3,2,5,2,[[2,5],[2,7,6,5]]]
           ]

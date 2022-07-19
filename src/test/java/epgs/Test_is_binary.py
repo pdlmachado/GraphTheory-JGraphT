d0a160e08ee594132f3dc0c728b0ed2b4903a972
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2022-07-19 09:57:16 -0300

    Create Test_is_binary.py

---
+++ src/test/java/epgs/Test_is_binary.py
@@ -0,0 +1,50 @@
+import unittest
+# From: https://eli.thegreenplace.net/2011/08/02/python-unit-testing-parametrized-test-cases
+class ParametrizedTestCase(unittest.TestCase):
+    """ TestCase classes that want to be parametrized should
+        inherit from this class.
+    """
+    def __init__(self, methodName='runTest', param=None):
+        super(ParametrizedTestCase, self).__init__(methodName)
+        self.param = param
+
+    @staticmethod
+    def parametrize(testcase_klass, param=None):
+        """ Create a suite containing all tests taken from the given
+            subclass, passing them the parameter 'param'.
+        """
+        testloader = unittest.TestLoader()
+        testnames = testloader.getTestCaseNames(testcase_klass)
+        suite = unittest.TestSuite()
+        for name in testnames:
+            suite.addTest(testcase_klass(name, param=param))
+        return suite
+      
+# Test Data
+import jgrapht
+
+d1 = jgrapht.create_graph(directed=True, weighted=False, dag=True)
+d1.add_vertices_from([x for x in range(0,6)])
+d1.add_edge(0,1,edge=0)
+d1.add_edge(0,2,edge=1)
+d1.add_edge(2,3,edge=2)
+d1.add_edge(2,4,edge=3)
+d1.add_edge(4,5,edge=4)
+
+d2 = jgrapht.create_graph(directed=True, weighted=False, dag=True)
+d2.add_vertices_from([x for x in range(0,6)])
+d2.add_edge(0,1,edge=0)
+d2.add_edge(0,2,edge=1)
+d2.add_edge(0,3,edge=2)
+d2.add_edge(2,4,edge=3)
+d2.add_edge(4,5,edge=4)
+
+d3 = jgrapht.create_graph(directed=True, weighted=False, dag=True)
+d3.add_vertex(20)
+
+class Test_is_binary_base(ParametrizedTestCase):
+  def test_base (self):
+    f,d,r = self.param
+    result = f(d)      
+
+params = [[d1,True],[d2,False],[d3,True]]

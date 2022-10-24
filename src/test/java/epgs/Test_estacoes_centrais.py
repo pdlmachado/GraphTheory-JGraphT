a66c489bcf4eb3890bcbf1187ebfec2b00c676ee
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2022-10-24 15:57:27 -0300

    Update Test_estacoes_centrais.py

--- src/test/java/epgs/Test_estacoes_centrais.py
+++ src/test/java/epgs/Test_estacoes_centrais.py
@@ -1,6 +1,30 @@
-# Definindo os dados para o teste
-# [grafo,dicionário de arestas, limiar,saída esperada]
+
 import jgrapht
+# Funções e pacotes para teste
+import unittest
+# From: https://eli.thegreenplace.net/2011/08/02/python-unit-testing-parametrized-test-cases
+class ParametrizedTestCase(unittest.TestCase):
+    """ TestCase classes that want to be parametrized should
+        inherit from this class.
+    """
+    def __init__(self, methodName='runTest', param=None):
+        super(ParametrizedTestCase, self).__init__(methodName)
+        self.param = param
+
+    @staticmethod
+    def parametrize(testcase_klass, param=None):
+        """ Create a suite containing all tests taken from the given
+            subclass, passing them the parameter 'param'.
+        """
+        testloader = unittest.TestLoader()
+        testnames = testloader.getTestCaseNames(testcase_klass)
+        suite = unittest.TestSuite()
+        for name in testnames:
+            suite.addTest(testcase_klass(name, param=param))
+        return suite
+
+# Definindo os dados para o teste
+# [grafo,dicionário de arestas, limiar,saída esperada]      
 g1 = jgrapht.create_graph(directed=False,weighted=True, 
                          allowing_multiple_edges=True)
 v_g1 = {}

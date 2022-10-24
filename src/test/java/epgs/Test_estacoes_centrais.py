38ba18dd772fe49ca61b9bd9eb06d396bdd71840
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2022-10-24 20:11:34 -0300

    Update Test_estacoes_centrais.py

--- src/test/java/epgs/Test_estacoes_centrais.py
+++ src/test/java/epgs/Test_estacoes_centrais.py
@@ -26,31 +26,31 @@ class ParametrizedTestCase(unittest.TestCase):
 # Test Data [k,saída desejada]
 # Grafo g1  
 params_g1 = [
-    [0,{0: 2, 1: 1, 2: 3, 3: 4, 4: 1, 5: 2, 6: 3, 7: 2, 8: 2}],
-    [1,{0: 2, 1: 1, 2: 3, 3: 4, 4: 1, 5: 2, 6: 3, 7: 2, 8: 2}],
-    [2,{0: 2, 2: 3, 3: 4, 5: 2, 6: 3, 7: 2, 8: 2}],
-    [3,{2: 3, 3: 4, 6: 3}],
-    [4,{3: 4}],
-    [5,{}],
+    [0,{0: 2, 1: 1, 2: 3, 3: 4, 4: 1, 5: 2, 6: 3, 7: 2, 8: 2},"#1"],
+    [1,{0: 2, 1: 1, 2: 3, 3: 4, 4: 1, 5: 2, 6: 3, 7: 2, 8: 2},"#2"],
+    [2,{0: 2, 2: 3, 3: 4, 5: 2, 6: 3, 7: 2, 8: 2},"#3"],
+    [3,{2: 3, 3: 4, 6: 3},"#4"],
+    [4,{3: 4},"#5"],
+    [5,{},"#6"],
 ]
 
 # Grafo lu
 params_lu = [
-    [6,{145: 6}],
-    [5,{11: 5, 145: 6}]
+    [6,{145: 6},"#7"],
+    [5,{11: 5, 145: 6},"#8"]
 ]
 
 # Classe de testes base
 class Test_estacoes_centrais_base (ParametrizedTestCase):
   def test_base (self):
-    f,g,e_g,k,saida_esperada = self.param
-    self.assertEqual(f(g,e_g,k),saida_esperada)
+    f,g,e_g,k,saida_esperada,m = self.param
+    self.assertEqual(f(g,e_g,k),saida_esperada,m)
 
 # Classe de testes edge
 class Test_estacoes_centrais_edge (ParametrizedTestCase):
   def test_edge (self):
     f,g,e_g = self.param
-    self.assertTrue(f(None,{},0) is None)
-    self.assertTrue(f(jgrapht.create_graph(),{},0) == {})
-    self.assertTrue(f(g,{},0) is None)
-    self.assertTrue(f(g,e_g,-1) is None)
+    self.assertTrue(f(None,{},0) is None,"Grafo não pode ser None")
+    self.assertTrue(f(jgrapht.create_graph(),{},0) == {},"Deve retornar {} para grafo nulo")
+    self.assertTrue(f(g,{},0) is None,"Dicionário não pode ser vazio para grafo não vazio")
+    self.assertTrue(f(g,e_g,-1) is None,"K tem que ser um número positivo ou zero")

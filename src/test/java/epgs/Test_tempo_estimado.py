0ab649f086e94430bbf5f9c15a30399f519b7c6f
pdlmachado <40029758+pdlmachado@users.noreply.github.com>
2022-11-01 09:16:40 -0300

    Update Test_tempo_estimado.py

--- src/test/java/epgs/Test_tempo_estimado.py
+++ src/test/java/epgs/Test_tempo_estimado.py
@@ -23,77 +23,75 @@ class ParametrizedTestCase(unittest.TestCase):
             suite.addTest(testcase_klass(name, param=param))
         return suite
     
-#####################################
+#####################
 # Test Data 
 
-# Valores de vértice origem,destino (graphpath) e t para grafo g1 e e_g1
-# Path é criado no método de teste
+# Valores de listas de estações para grafo g1 e e_g1
 params_g1 = [
-  [1,4,5,15,"#01"],
-  [0,2,10,11,"#02"],
-  [1,1,10,0,"#03"],
-  [1,2,10,11,"#04"],
-  [3,6,10,12,"#05"],
-  [7,8,7,34,"#06"],
-  [7,8,0,13,"#07"],
-  [7,8,1,16,"#08"],
+  [["Cruzeiro"],False,"#01"],
+  [["Centro"],True,"#02"],
+  [["Centenário","São José"],True,"#3"],
+  [["Nações","Bodocongó"],False,"#4"],
+  [["Prata","Centro"],True,"#5"],
+  [["Catolé","Centro","Catolé"],True,"#6"],
+  [[],False,"#7"],
 ]
 
-# Valores de vértice origem,destino (graphpath) e t para grafo lu e e_lu
+# Valores de listas de estações para grafo g1 e e_g1
 params_lu = [
-  [1,4,5,86,"#01"],
-  [131,2,10,140,"#02"],
-  [1,200,9,130,"#03"],
-  [1,200,0,58,"#04"],
-  [1,1,10,0,"#05"]  
+  [["South Kensington", "Baker Street", "Oxford Circus"],True,"#01"],
+  [["Bank"],False,"#02"],
+  [["Wimbledon","Beckton"],False,"#03"],
+  [["Upminster","Upminster Bridge"],False,"#04"],
+  [["Knightsbridge","Hyde Park Corner"],False,"#05"],
+  [["Marylebone","Upney"],True,"#06"],
+  [[],False,"#07"]
 ]
 
 # Outros exemplos de grafos para teste
 g2 = jgrapht.create_graph(directed=False)
 g2.add_vertices_from([0,1])
 g2.add_edge(0,1,edge=0)
-e_g2 = {0:{'line':'0'}}
-p2 = next(yen_k_loopless(g2,0,1,1))
+v_g2 = {0:{'label':'v0'},1:{'label':'v1'},}
 
 g3 = jgrapht.create_graph(directed=False)
 g3.add_vertex(0)
-e_g3 = {}
+v_g3 = {0:{'label':"v0"}}
 
 # Classe de testes base
-class Test_tempo_estimado_base (ParametrizedTestCase):
+class Test_quebradeservico_base (ParametrizedTestCase):
   def test_base (self):
-    f,g,e_g,v1,v2,t,saida_esperada,m = self.param
-    path = next(yen_k_loopless(g,v1,v2,1))
-    self.assertEqual(f(g,e_g,path,t),saida_esperada,m)
+    f,g,v_g,l,saida_esperada,m = self.param
+    self.assertEqual(f(g,v_g,l),saida_esperada,m)
 
 # Classe de testes edge
-class Test_tempo_estimado_edge (ParametrizedTestCase):
+class Test_quebradeservico_edge (ParametrizedTestCase):
   def test_None (self):
-    f,g,e_g = self.param
-    self.assertTrue(f(None,e_g2,p2,0) is None,"Grafo não pode ser None")
-    self.assertTrue(f(g2,None,p2,0) is None,"Dicionário de arestas não pode ser None")
+    f,g,v_g = self.param
+    self.assertTrue(f(None,v_g,[]) is None,"Grafo não pode ser None")
+    self.assertTrue(f(g,None,[]) is None,"Dicionário de arestas não pode ser None")
 
   def test_nulo (self):
-    f,g,e_g = self.param
-    self.assertTrue(f(jgrapht.create_graph(),{},p2,0) is None,"Deve retornar None para grafo nulo")
-    self.assertTrue(f(g2,{},e_g2,10) is None,"Dicionário não pode ser vazio para grafo não vazio")  
+    f,g,v_g = self.param
+    self.assertTrue(f(jgrapht.create_graph(),{},[]),"Deve retornar True para grafo nulo")
+    self.assertTrue(f(g,{},[]) is None,"Dicionário não pode ser vazio para grafo não vazio")  
 
-  def test_t_invalido (self):
-    f,g,e_g = self.param
-    self.assertTrue(f(g2,e_g2,p2,-1) is None,"t tem que ser um número positivo ou zero")
+  def test_stations_invalido (self):
+    f,g,v_g = self.param
+    self.assertTrue(f(g,v_g,["invalido"]) is None,"Estações devem estar definidas no grafo")
 
   def test_edict_invalido (self):
-    f,g,e_g = self.param
-    self.assertTrue(f(g,e_g2,p2,0) is None,"Dicionário inválido")
-
-  def test_path_invalido (self):
-    f,g,e_g = self.param
-    self.assertTrue(f(g,e_g,p2,0) is None,"Path não pertence a este grafo")
+    f,g,v_g = self.param
+    self.assertTrue(f(g,v_g2,[]) is None,"Dicionário inválido")
 
   def test_trivial (self):
-    f,g,e_g = self.param
-    self.assertTrue(f(g3,e_g3,None,3) is None,"Deve retornar None para grafo trivial")
+    f,g,v_g = self.param
+    self.assertTrue(f(g3,v_g3,["v0"]),"Grafo trivial fica nulo após remoção")
 
   def test_k2 (self):
-    f,g,e_g = self.param
-    self.assertEqual(f(g2,e_g2,p2,2),3) 
+    f,g,v_g = self.param
+    self.assertFalse(f(g2,v_g2,["v0"])) 
+
+  def test_todasasestacoes (self):
+    f,g,v_g = self.param
+    self.assertTrue(f(g,v_g,[v_g[v]['label'] for v in g.vertices])) 
